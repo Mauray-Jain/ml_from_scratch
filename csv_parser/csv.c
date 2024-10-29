ca9fd2fd9b6fe66b932485667b38d59b67d82b1c
Mauray-Jain <maurayjain@gmail.com>
2024-10-29 11:46:33 +0530

    parser done

--- csv_parser/csv.c
+++ csv_parser/csv.c
@@ -32,22 +32,26 @@ int parse(FILE* src, DataFrame* df) {
 	long pos = ftell(src);
 	size_t num_rows = 0;
 
-	while (!fgets(buf, LINE_MAX, src)) num_rows++;
+	while (fgets(buf, LINE_MAX, src)) num_rows++;
 	fseek(src, pos, SEEK_SET);
 
 	df->len_rows = num_rows;
-	printf("%lu\n", df->len_rows);
-	df->data = malloc(sizeof(double*) * df->len_rows);
+	df->data = malloc(sizeof(double*) * df->len_cols);
+	for (size_t i = 0; i < df->len_cols; i++) {
+		df->data[i] = malloc(sizeof(double) * df->len_rows);
+	}
 
-	for (size_t i = 0; i < df->len_rows; i++) {
-		fgets(buf, LINE_MAX, src);
-		df->data[i] = malloc(sizeof(double) * df->len_cols);
-		char *end, *p;
+	size_t row = 0, col = 0;
+	while (fgets(buf, LINE_MAX, src)) {
+		char *end, *p = buf;
 		for (double f = strtod(p, &end); p != end; f = strtod(p, &end)) {
-			df->data[i][j] = f;
+			df->data[col][row] = f;
+			col++;
 			p = end;
-			printf("%lf\n", f);
+			p++;
 		}
+		col = 0;
+		row++;
 	}
 	return 0;
 }
@@ -57,10 +61,16 @@ int main(void) {
 	DataFrame df = {0};
 	parse(src, &df);
 	/*printf("%lu\n", df.len_cols);*/
+	printf("%lu, %lu\n", df.len_cols, df.len_rows);
 	for (size_t i = 0; i < df.len_cols; i++) {
 		/*printf(",%s,\n", df.col_names[i].ptr);*/
+		for (size_t j = 0; j < df.len_rows; j++) {
+			printf("[%lu][%lu] = %lf\n", i, j, df.data[i][j]);
+		}
 		free(df.col_names[i].ptr);
+		free(df.data[i]);
 	}
+	free(df.data);
 	fclose(src);
 	return 0;
 }
